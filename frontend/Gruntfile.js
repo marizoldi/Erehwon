e485ac77ec50a1bef471330840514fc8ddd12784
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-05-28 10:38:10 +0100

    Removed unused grunt plugins

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,20 +1,10 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
 grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
-	//------- CSS Minify -------//
-	cssmin: {
-		combine: {
-		  files: {
-		    '/static/styles/screen.css': ['styles/css/styles.css']
-		  }
-		}
-	},
 	//------- SASS -------//
 	sass: {
 		dist: {
