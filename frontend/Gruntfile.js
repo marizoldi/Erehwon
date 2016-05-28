670b0db36385993a2c35fd9e3be13c7c32281390
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-05-28 10:35:19 +0100

    Added grunt sass and grunt watch

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	//------- CSS Minify -------//
+	cssmin: {
+		combine: {
+		  files: {
+		    '/static/styles/screen.css': ['styles/css/styles.css']
+		  }
+		}
+	},
+	//------- SASS -------//
+	sass: {
+		dist: {
+			files: {
+				'styles/css/styles.css': 'styles/styles.scss'
+			}
+		}
+	},
+	//------- Watch SASS -> CSS -------//
+	watch: {
+		sass: {
+		  files: 'styles/styles.scss',
+		  tasks: ['sass']
+		}
+	},
+
+});
+
+	grunt.registerTask('default', ['sass']);
+
+}
