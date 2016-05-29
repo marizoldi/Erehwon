aa5e85ab7b9fcb126d89fe0d9221b40556cdc4d7
mariza <marizadima@yahoo.com>
2016-05-29 17:23:00 +0100

    Models messages

--- erehwon/profiles/admin.py
+++ erehwon/profiles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Idea
+from .models import Project, Idea, Message
 
 admin.site.register(Project)
 admin.site.register(Idea)
+admin.site.register(Message)
