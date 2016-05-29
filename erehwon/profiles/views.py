1c98bc95625531fecc79d8a8f01b02d388acc542
mariza <marizadima@yahoo.com>
2016-05-29 18:25:32 +0100

    Choose a random daily message and show it on login

--- erehwon/profiles/views.py
+++ erehwon/profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from profiles.models import Message
 
 # Create your views here.
 class LoginView(TemplateView):
@@ -21,5 +21,10 @@ class DashboardView(TemplateView):
     """
     The Dashboard view as soon as user logs in.
     """
+    def get_daily_message(self):
+        """Returns message list from DB"""
+    	all_messages = Message.objects.all()
+        random_msg = random.sample(items, 1)
+        return random_msg
 
     template_name = "profiles/dashboard-logged-in.html"
