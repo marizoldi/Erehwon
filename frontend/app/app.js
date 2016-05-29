0f0afa43725a7ee4310cd0306f9beef7b8d7a9f4
neraks <karenfish.uk@gmail.com>
2016-05-29 16:50:30 +0100

    Implement designed projects page
    Added skeleton css framework

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,3 +3,11 @@ var erehwonApp = angular.module('erehwonApp',[]);
 erehwonApp.controller('DashboardController', function DashboardController($scope) {
 	
 });
+
+erehwonApp.controller('ManageController', function ManageController($scope){
+	$scope.showAdd = false;
+	$scope.clickAdd = function() {
+		$scope.showAdd = !$scope.showAdd;
+	}
+	$scope.addForm = {};
+});
