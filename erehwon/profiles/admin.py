a6cea1b9708cf579887bad1fcf9ab76c0d589557
mariza <marizadima@yahoo.com>
2016-05-29 15:32:02 +0100

    Resolved DB issues

--- erehwon/profiles/admin.py
+++ erehwon/profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import Project, Idea
 
 admin.site.register(Project)
+admin.site.register(Idea)
