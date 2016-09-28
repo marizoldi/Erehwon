f582703f44f479ad49555d154224562a3a564ce7
mariza <marizadima@yahoo.com>
2016-09-28 10:42:10 +0100

    Django custom registration

---
+++ erehwon/profiles/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from profiles.models import Project, ErehwonUser
+
+from registration.forms import RegistrationForm
+
+class ProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ('title','synopsis')
+
+class ErehwonUserSignUpForm(RegistrationForm):
+
+        model = ErehwonUser
+        fields = ('username', 'email','password1', 'password1')
