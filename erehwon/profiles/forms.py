19e58c85f17df3540c3f4c09ada95df60dfe034a
mariza <marizadima@yahoo.com>
2016-10-07 00:24:36 +0100

    UI designs cleanup

--- erehwon/profiles/forms.py
+++ erehwon/profiles/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from profiles.models import Project
 
-from registration.forms import RegistrationForm
 
 class ProjectForm(forms.ModelForm):
 
     class Meta:
         model = Project
-        fields = ('title','synopsis')
+        fields = ('title', 'label')
