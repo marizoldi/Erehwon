07cc46699c844cb4b5f6c8c5f1271f09be3b15cb
mariza <marizadima@yahoo.com>
2016-11-29 12:14:07 +0000

    Changed prod for verbose logging

--- erehwon/erehwon/settings/prod.py
+++ erehwon/erehwon/settings/prod.py
@@ -3,5 +3,62 @@ import dj_database_url
 
 DEBUG = False
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALLOWED_HOSTS = ['erehwon.herokuapp.com']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'fashrevwall': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+# Registration settings
+
+ACCOUNT_ACTIVATION_DAYS = 7
+EXPIRATION_DAYS = 5
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ["GMAIL_UN"]
+DEFAULT_FROM_EMAIL = 'projecterehwon@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ["GMAIL_PW"]
+EMAIL_USE_TLS = True
