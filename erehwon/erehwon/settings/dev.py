1ee18e3c7d90745db5cf55db01172f89af33d1fb
Liliana Kastilio <lili@Lilianas-MacBook-Pro.local>
2017-01-04 21:23:44 +0000

    Fixes to url and using teh correct settings in dev

--- erehwon/erehwon/settings/dev.py
+++ erehwon/erehwon/settings/dev.py
@@ -1,16 +1,12 @@
 from erehwon.settings import *
-import dj_database_url
 
 DEBUG = True
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+SECRET_KEY = 'FakeforDevOnly'
 
-SECRET_KEY = os.environ["SECRET_KEY"]
-ALLOWED_HOSTS = ['erehwon-dev.herokuapp.com']
 
 LOGGING = {
     'version': 1,
