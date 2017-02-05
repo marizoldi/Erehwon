038fb84f480278d2b95ce8b06ff0c2c162497d1b
mariza <marizadima@yahoo.com>
2017-02-05 17:05:38 +0000

    Solved password change error

--- erehwon/erehwon/settings/__init__.py
+++ erehwon/erehwon/settings/__init__.py
@@ -85,7 +85,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': (
-            os.path.join(BASE_DIR, 'erehwon/templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ),
         'APP_DIRS': True,
         'OPTIONS': {
