8fb9a508266e6a26ab34f64c591d3f87cc655f82
mariza <marizadima@yahoo.com>
2017-02-18 10:37:54 +0000

    Removed $ symbol

--- erehwon/erehwon/urls.py
+++ erehwon/erehwon/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^$', HomepageView.as_view(), name="index"),
     url(r'^accounts/register/$', RegistrationView.as_view(form_class=ErehwonUserSignUpForm), name="registration_register"),
     # This line includes automatically all views and urls for registration/activation/password reset
-    url(r'^accounts/', include('registration.backends.hmac.urls')),  
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^accounts/logout/$', logout_view, name="logout_view"),
     # url(r'^dashboard', loggedin_view, name="dashboard"),
     url(r'^projects/$', project_list, name="project_list"),
@@ -30,12 +30,12 @@ urlpatterns = [
     # django-postman
     url(r'^messages/erehwon/$', MessagesIndexView.as_view(), name='MessagesIndex View'),
     url(r'^messages/messageprofile/$', MessagesProfileView.as_view(), name='MessagesProfile View'),
-    
+
     #TODO write some custom postman views
     #css classes here: http://django-postman.readthedocs.io/en/latest/views.html
     #and hook them up.
     url(r'^inbox/$', TemplateView.as_view(template_name='inbox.html')),
-    url(r'^messages/$', include('postman.urls', namespace='postman', app_name='postman')),
+    url(r'^messages/', include('postman.urls', namespace='postman', app_name='postman')),
     # django-notifications-hq
     url(r'^inbox/notifications/', include(notifications.urls, namespace='notifications')),
     url(r'^api/sendmessage/$', api_send_message, name='Api Send Message View'),
