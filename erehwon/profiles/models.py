51d9b52f159cf80d5a466bdb8fbf11760a606014
mariza <marizadima@yahoo.com>
2017-05-22 20:23:52 +0100

    more updates

--- erehwon/profiles/models.py
+++ erehwon/profiles/models.py
@@ -3,17 +3,6 @@ from django.utils import timezone
 
 from custom_user.models import AbstractEmailUser
 
-LABEL_OPTIONS = (
-    ('A','Activism'),
-    ('DA','Digital Activism'),
-    ('CD','Community Development'),
-    ('UP','Urban Planning'),
-    ('NE','New Ecologies'),
-    ('AE','Alternative Economies'),
-    ('CM','Citizen Movement'),
-    ('AI','Artistic Interventions'),
-)
-
 class ErehwonUser(AbstractEmailUser):
 #
 #     # basic information
@@ -32,9 +21,6 @@ class Project(models.Model):
    # contributors = models.CharField(max_length=20)
    active_status = models.BooleanField(default=1)
    is_added_to_map = models.BooleanField(default=0)
-   label = models.CharField(max_length=30,
-       choices=LABEL_OPTIONS,
-       default='A')
 
    def __str__(self):
        return self.title
@@ -42,7 +28,7 @@ class Project(models.Model):
 class Idea(models.Model):
 
     user = models.ForeignKey(ErehwonUser)
-    project = models.ForeignKey('profiles.Project')
+    # project = models.ForeignKey('profiles.Project')
     title = models.CharField(max_length=30)
     synopsis = models.TextField(max_length=300)
     # contributors = models.CharField(max_length=20)
@@ -59,7 +45,6 @@ class CallForAction(models.Model):
     action_location = models.CharField(max_length=30, default='Erehwon')
     date = models.DateTimeField(db_index=True, default=timezone.now)
     contributors = models.CharField(max_length=20, default='Erehwon')
-
 
     def __str__(self):
         return self.title
