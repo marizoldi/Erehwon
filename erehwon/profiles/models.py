8f1989a867d30aca4e364fa0343ed2bc37e4c921
leungant <leungant@a.com>
2017-07-04 20:02:40 +0200

    images and files stored in custom Storage object. currently files are stored in database.

--- erehwon/profiles/models.py
+++ erehwon/profiles/models.py
@@ -11,18 +11,59 @@ class ErehwonUser(AbstractEmailUser):
 def __str__(self):
         return self.username
 
+class File(models.Model):
+    filename = models.CharField(max_length=220)
+    file = models.BinaryField()
+    def __str__(self):
+        return self.filename
+
+
+from django.core.files.storage import Storage
+from django.utils.deconstruct import deconstructible # deals with serialization error in Project
+@deconstructible
+class FlexStorage(Storage):
+    def open(self, name, mode='rb'):
+        f = File.objects.filter(filename=name)
+        return f.read()
+    def _save(self, name, content): 
+        content.open() # reopen, file seek 0
+        f = File(filename=name, file=content.file.read())
+        f.save()
+        return name
+    def exists(self, name):
+        f_list = File.objects.filter(filename=name)
+        return len(f_list) > 0
+    def delete():
+        files = File.objects.filter(filename=name)
+        for f in files:
+            f.delete()
+    # def listdir():
+    # def size():
+    # def url():
+
+
+def get_storage():
+    return FlexStorage()
+
 class Project(models.Model):
+    user = models.ForeignKey(ErehwonUser)
+    title = models.CharField(max_length=30)
+    synopsis = models.TextField(max_length=300)
+    image = models.ImageField(storage=get_storage(), null=True, blank=True)
 
-   user = models.ForeignKey(ErehwonUser)
-   title = models.CharField(max_length=30)
-   synopsis = models.TextField(max_length=300)
-   material = models.URLField(max_length=300, blank=True)
-   contributors = models.CharField(max_length=20)
-   active_status = models.BooleanField(default=1)
-   is_added_to_map = models.BooleanField(default=0)
+    files = models.ForeignKey(File, on_delete=models.CASCADE,
+                                 null=True, blank=True)
+    material = models.URLField(max_length=300, blank=True)
+    contributors = models.CharField(max_length=20)
+    active_status = models.BooleanField(default=1)
+    is_added_to_map = models.BooleanField(default=0)
+ 
+    def __str__(self):
+        return self.title
 
-   def __str__(self):
-       return self.title
+class ProjectFile(models.Model):
+    file = models.OneToOneField(File, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 class Idea(models.Model):
 
